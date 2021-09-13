232f980f486fb7af17ec99a9cbbf274f3b86384f
tagimoucia-cheryl <45718615+tagimoucia-cheryl@users.noreply.github.com>
2021-09-13 20:04:21 +0100

    Create lowest systolic BP.R

---
+++ R_datapipeline/observations/work_in_progress/filter_24/lowest systolic BP.R
@@ -0,0 +1,24 @@
+#creates data frame with 2 columns: hospital visit id and lowest SBP in first 24 hours
+
+#SETUP
+library(stringr)
+
+#Load observations file
+x <- file.choose()
+day <- read.csv(x)
+
+View(day)
+#Retrieve BP measurements only
+SBP <-
+  subset(day, id_in_application == 5)
+
+View(SBP)
+#Create new variable column containing Systolc BP values only
+SBP$sbp<-
+str_extract(SBP$value_as_text, "\\d+") 
+
+df_LSYS<-
+  SBP %>%
+  dplyr::group_by(hospital_visit_id) %>%
+  dplyr::summarise(LSYS = min(sbp)) #table with min systolic BP
+#for each specific hospital_visit(in pre-requisite ITU split locale)
