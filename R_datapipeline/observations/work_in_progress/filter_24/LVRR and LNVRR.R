adf42af8d850f5fab5f1074c5a3e8cdaa5b7995a
tagimoucia-cheryl <45718615+tagimoucia-cheryl@users.noreply.github.com>
2021-09-15 20:12:18 +0100

    Create LVRR and LNVRR.R

---
+++ R_datapipeline/observations/work_in_progress/filter_24/LVRR and LNVRR.R
@@ -0,0 +1,42 @@
+library(tidyverse)
+
+x <- file.choose()#choose "vent_rr_24.csv" (or equivalent if saved differently)
+RRv <- read.csv(x)
+
+#concatenate hospital visit id with observation_datetime to make unique observation
+#time point for specifc patient (otherwise filtering by datetime alone may refer to
+#different patient)
+uRRv <- RRv
+uRRv$HO <-
+  paste(uRRv$hospital_visit_id, uRRv$observation_datetime)
+
+#split the vent from non vent
+vent <-
+  subset(uRRv, value_as_text == "Yes")
+nonv <-
+  subset(uRRv, value_as_text == "No")
+
+#create data frames with RR of ventilated and non-ventilated patients separately
+vent_rr <-
+  filter(uRRv, uRRv$HO %in% vent$HO)
+nonv_rr <-
+  filter(uRRv, uRRv$HO %in% nonv$HO)
+
+#remove 'na' rows from both vent and non vent rr data frames (otherwise will interfere with identifying minimum value)
+f_vent_rr <-
+  vent_rr %>%
+  filter(!is.na(value_as_real))
+f_nonv_rr <- 
+  nonv_rr %>%
+  filter(!is.na(value_as_real))
+
+#then identify minimum RR from each
+min_vent <-
+  f_vent_rr %>%
+  group_by(hospital_visit_id) %>%
+  summarise(LVRR = min(value_as_real))
+min_nv <-
+  f_nonv_rr %>%
+  group_by(hospital_visit_id) %>%
+  summarise(LNVRR = min(value_as_real))
+
