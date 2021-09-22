5fe49c2734a23529588ffceb1dca54864a74ef3f
tagimoucia-cheryl <45718615+tagimoucia-cheryl@users.noreply.github.com>
2021-09-22 15:07:08 +0100

    Update LSYS.R

--- R_datapipeline/observations/work_in_progress/filter_24/LSYS.R
+++ R_datapipeline/observations/work_in_progress/filter_24/LSYS.R
@@ -17,8 +17,44 @@ View(SBP)
 SBP$sbp<-
 str_extract(SBP$value_as_text, "\\d+") 
 
+#identify "first admisison"
+f_at <- 
+SBP %>%
+  group_by(hospital_visit_id) %>%
+  summarise(f_at = min(admission_time))
+
+#merge first admission with pre-existing HR
+f_SBP <- 
+merge(x = SBP, y = f_at, by = "hospital_visit_id", all = TRUE)
+
+#translate admission time to POSIXlt
+f_SBP$f_at <- substr(
+  f_SBP$f_at,1,19
+)
+f_SBP$f_at <- strptime(
+ f_SBP$f_at,"%d/%m/%Y %T", tz = "GMT"
+) 
+
+#new at column for admission datetime + 24hrs in POSIXlt format
+f_SBP$f_at24 <- f_SBP$f_at +
+  dhours(24)
+
+#create observation datetime column in POSIXlt format
+f_SBP$ot <- 
+  substr(f_SBP$observation_datetime,1,19)
+
+f_SBP$ot <-
+  strptime(
+    f_SBP$ot,"%d/%m/%Y %T", tz = "GMT") 
+
+#filter data frame for observations done within first 24 hours of admisison
+SBP_24 <- 
+  f_HR%>%
+  filter(f_SBP$ot > f_SBP$f_at, f_SBP$ot < f_SBP$f_at24)
+
+
 df_LSYS<-
-  SBP %>%
+  SBP_24 %>%
   dplyr::group_by(hospital_visit_id) %>%
   dplyr::summarise(LSYS = min(sbp)) #table with min systolic BP
 #for each specific hospital_visit(in pre-requisite ITU split locale)
